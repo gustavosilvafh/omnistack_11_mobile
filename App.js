557823791d79cec525ba10572893e48e23d20203
Gustavo Silva <gustavo@MacBook-Pro-de-Gustavo.local>
2020-03-26 15:35:56 -0300

    Configurated files structure

--- App.js
+++ App.js
@@ -3,17 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View >
       <Text>Puta que bagulho massa</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
